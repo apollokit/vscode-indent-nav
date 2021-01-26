664d74ab7a3b9cdcec2cdc2db82b030b97106e6e
Kit Kennedy <kitkennedy8@gmail.com>
2021-01-25 18:27:38 -0800

    got updated logic for indent block hopping working

--- src/extension.ts
+++ src/extension.ts
@@ -9,66 +9,68 @@ import { setFlagsFromString } from 'v8';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let indentNav = new IndentNav();
-	console.log('indent-nav is now active!');
+	console.log('indent-nav-kit is now active!');
+	vscode.window.showInformationMessage('indent-nav-kit is now active!');
 
-	let wordRight = vscode.commands.registerCommand('indent-nav.wordRight', () => {
-		indentNav.moveByWord(1);
-	});
-	context.subscriptions.push(wordRight);
-	let wordLeft = vscode.commands.registerCommand('indent-nav.wordLeft', () => {
-		indentNav.moveByWord(-1);
-	});
-	context.subscriptions.push(wordLeft);
+	// let wordRight = vscode.commands.registerCommand('indent-nav.wordRight', () => {
+	// 	indentNav.moveByWord(1);
+	// });
+	// context.subscriptions.push(wordRight);
+	// let wordLeft = vscode.commands.registerCommand('indent-nav.wordLeft', () => {
+	// 	indentNav.moveByWord(-1);
+	// });
+	// context.subscriptions.push(wordLeft);
 
 	let nextSibling = vscode.commands.registerCommand('indent-nav.nextSibling', () => {
-		indentNav.jumpToIndent(1, 0);
+		indentNav.jumpToIndent();
 	});
 	context.subscriptions.push(nextSibling);
 	let previousSibling = vscode.commands.registerCommand('indent-nav.previousSibling', () => {
-		indentNav.jumpToIndent(-1, 0);
+		indentNav.jumpToIndent(false);
 	});
 	context.subscriptions.push(previousSibling);
-	let nextSiblingForce = vscode.commands.registerCommand('indent-nav.nextSiblingForce', () => {
-		indentNav.jumpToIndent(1, 0, true);
-	});
-	context.subscriptions.push(nextSiblingForce);
-	let previousSiblingForce = vscode.commands.registerCommand('indent-nav.previousSiblingForce', () => {
-		indentNav.jumpToIndent(-1, 0, true);
-	});
-	context.subscriptions.push(previousSiblingForce);
-	let lastSibling = vscode.commands.registerCommand('indent-nav.lastSibling', () => {
-		indentNav.jumpToIndent(1, 0, false, true);
-	});
-	context.subscriptions.push(lastSibling);
-	let firstSibling = vscode.commands.registerCommand('indent-nav.firstSibling', () => {
-		indentNav.jumpToIndent(-1, 0, false, true);
-	});
-	context.subscriptions.push(firstSibling);
+	
+	// let nextSiblingForce = vscode.commands.registerCommand('indent-nav.nextSiblingForce', () => {
+	// 	indentNav.jumpToIndent(1, 0, true);
+	// });
+	// context.subscriptions.push(nextSiblingForce);
+	// let previousSiblingForce = vscode.commands.registerCommand('indent-nav.previousSiblingForce', () => {
+	// 	indentNav.jumpToIndent(-1, 0, true);
+	// });
+	// context.subscriptions.push(previousSiblingForce);
+	// let lastSibling = vscode.commands.registerCommand('indent-nav.lastSibling', () => {
+	// 	indentNav.jumpToIndent(1, 0, false, true);
+	// });
+	// context.subscriptions.push(lastSibling);
+	// let firstSibling = vscode.commands.registerCommand('indent-nav.firstSibling', () => {
+	// 	indentNav.jumpToIndent(-1, 0, false, true);
+	// });
+	// context.subscriptions.push(firstSibling);
 
-	let nextChild = vscode.commands.registerCommand('indent-nav.nextChild', () => {
-		indentNav.jumpToIndent(1, 1);
-	});
-	context.subscriptions.push(nextChild);
-	let previousChild = vscode.commands.registerCommand('indent-nav.previousChild', () => {
-		indentNav.jumpToIndent(-1, 1);
-	});
-	context.subscriptions.push(previousChild);
-	let previousParent = vscode.commands.registerCommand('indent-nav.previousParent', () => {
-		indentNav.jumpToIndent(-1, -1, true);
-	});
-	context.subscriptions.push(previousParent);
-	let nextParent = vscode.commands.registerCommand('indent-nav.nextParent', () => {
-		indentNav.jumpToIndent(1, -1, true);
-	});
-	context.subscriptions.push(nextParent);
-	let startSelection = vscode.commands.registerCommand('indent-nav.startSelection', () => {
-		indentNav.startSelection();
-	});
-	context.subscriptions.push(startSelection);
-	let finishSelection = vscode.commands.registerCommand('indent-nav.finishSelection', () => {
-		indentNav.finishSelection();
-	});
-	context.subscriptions.push(finishSelection);
+	// let nextChild = vscode.commands.registerCommand('indent-nav.nextChild', () => {
+	// 	indentNav.jumpToIndent(1, 1);
+	// });
+	// context.subscriptions.push(nextChild);
+	// let previousChild = vscode.commands.registerCommand('indent-nav.previousChild', () => {
+	// 	indentNav.jumpToIndent(-1, 1);
+	// });
+	// context.subscriptions.push(previousChild);
+	// let previousParent = vscode.commands.registerCommand('indent-nav.previousParent', () => {
+	// 	indentNav.jumpToIndent(-1, -1, true);
+	// });
+	// context.subscriptions.push(previousParent);
+	// let nextParent = vscode.commands.registerCommand('indent-nav.nextParent', () => {
+	// 	indentNav.jumpToIndent(1, -1, true);
+	// });
+	// context.subscriptions.push(nextParent);
+	// let startSelection = vscode.commands.registerCommand('indent-nav.startSelection', () => {
+	// 	indentNav.startSelection();
+	// });
+	// context.subscriptions.push(startSelection);
+	// let finishSelection = vscode.commands.registerCommand('indent-nav.finishSelection', () => {
+	// 	indentNav.finishSelection();
+	// });
+	// context.subscriptions.push(finishSelection);
 
 }
 
@@ -84,7 +86,7 @@ class IndentNav {
 		editor.selection = new vscode.Selection(newPosition, newPosition);
 		editor.revealRange(new vscode.Range(newPosition, newPosition));
 	}
-	public getLevel(line: any) {
+	public getLevel(line: any): number{
 		if (line.text.toString().trimRight().length === 0) {
 			return -1;
 		} else {
@@ -92,67 +94,108 @@ class IndentNav {
 		}
 	}
 
-	public jumpToIndent(lineIncrement: number, levelSign: number, unbounded:boolean = false, jumpToLast:boolean=false) {
+	/**
+	* Jump to a desired location, based on indent block, from current location
+	*
+	* @remarks
+	* 
+	*
+	* @param indentBlockIncrement - number of indent blocks to jump. Will first jump to beginning/end of current block, then to the next one
+	* @param y - The second input number
+	* @returns The arithmetic mean of `x` and `y`
+	*
+	* @beta
+	*/
+	public jumpToIndent(forwards: boolean = true) {
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return;
 		}
-		let n = editor.document.lineCount;
-		let i = editor.selection.start.line;
-		let line = editor.document.lineAt(i);
-		let originalLevel = this.getLevel(line);
-		let isBlank = ( originalLevel < 0);
-		if (isBlank) {
-			return;
+		let numLines = editor.document.lineCount;
+
+		// current line being considered in the seeking code below
+		let iCurrLine = editor.selection.start.line;
+		let iLastLine = iCurrLine;
+
+		// original level - the indent level that we are trying to match
+		// use the position of the cursor as the original level
+		let originalLevel = editor.selection.active.character;
+		// though, if we're past the first non whitespace character of the line, then use that character
+		let line = editor.document.lineAt(iCurrLine);
+		let firstChar = this.getLevel(line);
+		originalLevel = Math.min(firstChar, originalLevel);
+		// if we're on a blank line (level == -1) then just assume 0 (i.e. any line at lowest indent)
+		originalLevel = Math.max(originalLevel, 0);
+
+		let found = false;
+		// the line to jump to
+		let iJump = -1;
+
+		// if the previous line level matched the original level. Use this to keep track of whether 
+		// or not we are in an indent block. Assume we are not, to start
+		let prevWasMatch = false;
+		let firstStep = true;
+
+		let lineIncrement = 1;
+		if (!forwards) {
+			lineIncrement = -1;
 		}
-		let found:boolean = false;
-		let result:number = -1;
-		while (true) {
-			i += lineIncrement;
-			if ((i >= n) || (i < 0)) {
-				// Hit the end of the document - Not found
+		while (!found) {
+			iCurrLine += lineIncrement;
+
+			// check if we're immediately stepping off the beginning/end of the doc
+			if ((iCurrLine >= numLines) || (iCurrLine < 0)) {
 				break;
-			}
-			let line = editor.document.lineAt(i);
+			}			
+
+			let line = editor.document.lineAt(iCurrLine);
 			let level = this.getLevel(line);
-			isBlank = ( level < 0);
-			if (isBlank) {
-				continue;
-			}
-			if ((level < originalLevel) && (!unbounded)) {
-				// Hit the end of this indentation block - not found
+			// if we're at the same level as the original line
+			let levelMatches = level == originalLevel;
+			
+			// check first/last line of doc
+			if ((iCurrLine == numLines - 1) || (iCurrLine == 0)) {
+				if (levelMatches) {
+					iJump = iCurrLine;
+					found = true;
+				}
+				// if no match, then just leave us at the starting place
 				break;
 			}
-			if (Math.sign(level - originalLevel) == Math.sign(levelSign)) {
-				result = i;
+
+			// we're at the beginning of a new indent block at the original level - we should stop
+			if (levelMatches && !prevWasMatch && !firstStep) {
+				iJump = iCurrLine;
 				found = true;
-				if (!jumpToLast) {
-					break;
-				}
-				//vscode.window.showInformationMessage('Jumping to line ' + i);
-				//let newPosition = editor.selection.active.with(i, 0);
-				//editor.selection = new vscode.Selection(newPosition, newPosition);
-				//editor.revealRange(new vscode.Range(newPosition, newPosition));
-				//editor.show();
-				//vscode.window.showInformationMessage('Hello World!' + __dirname);
-				//home/tony/test
-				//return;
 			}
+			// we're off the end of an indent block at the original level - we should stop
+			if (!levelMatches && prevWasMatch) {
+				iJump = iLastLine;
+				found = true;
+			}
+
+			iLastLine = iCurrLine;
+			prevWasMatch = levelMatches;
+			firstStep = false;
 		}
+
 		if (found) {
-			this.jumpTo(result, 0);
-		} else {
+			this.jumpTo(iJump, originalLevel);
+		}
+		else {
 			let message = '';
-			if (lineIncrement > 0) {
+			if (forwards) {
 				message = 'No next';
-			} else {
+			}
+			else {
 				message = 'No previous';
 			}
 			vscode.window.showWarningMessage(message);
 		}
+
 	}
 
-	public moveByWord(increment:number) {
+	/* public moveByWord(increment:number) {
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return;
@@ -242,5 +285,5 @@ class IndentNav {
 		editor.selection = new vscode.Selection(position1, position2);
 		editor.revealRange(new vscode.Range(position1, position2));
 
-	}
+	} */
 }
